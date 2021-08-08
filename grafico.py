9a89f182c0b340b499d99606576c4277745daa0e
willlucas1512 <41302062+willlucas1512@users.noreply.github.com>
2021-08-08 00:45:48 -0300

    add runs

--- grafico.py
+++ grafico.py
@@ -6,11 +6,11 @@ CDataFrame = pd.read_csv('temposC.csv')
 
 figura, eixos = plt.subplots(1,2,figsize=(10,5), sharey=False)
 
-fortranDataFrame.plot.line(x = 'n', y = 'TempoIJ', ax = eixos[1], ylabel='Tempo em segundos')
+fortranDataFrame.plot.line(x = 'n', y = 'TempoIJ', ax = eixos[1])
 fortranDataFrame.plot.line(x = 'n', y = 'TempoJI', ax = eixos[1])
 eixos[1].set_title('Fortran',fontsize=12)
 
-CDataFrame.plot.line(x = 'n', y = 'TempoIJ', ax = eixos[0], ylabel='Tempo em segundos')
+CDataFrame.plot.line(x = 'n', y = 'TempoIJ', ax = eixos[0])
 CDataFrame.plot.line(x = 'n', y = 'TempoJI', ax = eixos[0])
 eixos[0].set_title('C',fontsize=12)
 
